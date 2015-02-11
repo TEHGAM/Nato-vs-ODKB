b846e34d88ae85249c05ba661d40c8828fb7df0b
a11archer <hromtcov_at_rus@mail.ru>
2015-02-11 19:49:03 +0300

    TAW_vd обновился. надо проверить

--- Nato vs ODKB with mods/Nato_vs_ODKB_PvP.Altis/scripts/taw_vd/CfgFunctions.hpp
+++ Nato vs ODKB with mods/Nato_vs_ODKB_PvP.Altis/scripts/taw_vd/CfgFunctions.hpp
@@ -3,7 +3,7 @@ class TAWVD
 	tag = "TAWVD";
 	class TAW_VD
 	{
-		file = "scripts\taw_vd";
+		file = "taw_vd";
 		class onSliderChange {};
 		class onTerrainChange {};
 		class updateViewDistance {};
